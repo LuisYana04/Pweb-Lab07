6aea8c32924a4ec683fbb0d9e65f42eaf96a8fec
Luis <lyanaa@unsa.edu.pe>
2023-08-08 22:25:34 -0500

    Modelos añadidos

---
+++ Apps/Aplicacion1/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class Aplicacion1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Aplicacion1'
+class UsersConfig(AppConfig):
+    name = 'Aplicacion1'
